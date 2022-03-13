0972317a2326b0110084adf603eccd0d28ea4dd7
Tony Tang <tonytang@uber.com>
2022-03-12 20:51:12 -0800

    Update DaysBaseModelWrapper.java and view holder.

--- app/src/main/java/com/example/android/localweatherwithfragment/Adapter/ViewHolder.java
+++ app/src/main/java/com/example/android/localweatherwithfragment/Adapter/ViewHolder.java
@@ -4,23 +4,43 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android.localweatherwithfragment.DataModel.DaysBaseModelWrapper;
 import com.example.android.localweatherwithfragment.R;
 
-public class ViewHolder extends RecyclerView.ViewHolder{
+public class ViewHolder extends RecyclerView.ViewHolder {
+    private final View layoutExpand;
     LinearLayout parent_layout;
-    TextView datetime_by_days,conditions_by_days,temp_max,temp_min;
+    TextView datetime_by_days, conditions_by_days, temp_max, temp_min;
     ImageView icon_by_days;
 
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
-
         parent_layout = itemView.findViewById(R.id.list_item);
         datetime_by_days = itemView.findViewById(R.id.datetime_by_days);
         conditions_by_days = itemView.findViewById(R.id.conditions_by_days);
         temp_max = itemView.findViewById(R.id.temp_max);
         temp_min = itemView.findViewById(R.id.temp_min);
         icon_by_days = itemView.findViewById(R.id.icon_by_days);
+        layoutExpand = itemView.findViewById(R.id.layoutExpand);
+    }
+
+
+    private void bindExpandAction(DaysBaseModelWrapper wrapper) {
+        itemView.setOnClickListener(view -> onItemToggled(wrapper, view));
+    }
+
+    private void onItemToggled(DaysBaseModelWrapper personModel, View view) {
+        boolean isExpanded = !personModel.isExpanded();
+        Animations.switchBackgroundColor(view, isExpanded);
+        if (isExpanded) {
+            Animations.expand(layoutExpand);
+        } else {
+            Animations.collapse(layoutExpand);
+        }
+        personModel.setExpanded(isExpanded);
     }
 }
---
+++ app/src/main/java/com/example/android/localweatherwithfragment/DataModel/DaysBaseModelWrapper.java
@@ -0,0 +1,26 @@
+package com.example.android.localweatherwithfragment.DataModel;
+
+public class DaysBaseModelWrapper {
+
+    public final DaysBaseModel daysBaseModel;
+    public boolean expanded;
+
+    public DaysBaseModel getDaysBaseModel() {
+        return daysBaseModel;
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
+
+    public DaysBaseModelWrapper(DaysBaseModel daysBaseModel, boolean expanded) {
+
+
+        this.daysBaseModel = daysBaseModel;
+        this.expanded = expanded;
+    }
+}
